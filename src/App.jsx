7871378361cf5f5c3e10454504afa3ae5ab78d40
Harshsharma008 <harshsharma01012002@gmail.com>
2023-12-10 20:51:34 +0530

    redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { fetchDataFromApi } from './Utils/api' // in {} because we use the export of the single method
 import { useEffect,useState } from 'react'
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfigration } from './store/homeSlice'
+
 
 function App() {
+  const dispatch = useDispatch();
+  const {url} = useSelector((state)=>
+    state.home
+  )
+  console.log(url);
   useEffect(()=>{
     apiTesting();
   },[])
@@ -11,12 +19,15 @@ function App() {
     fetchDataFromApi("/movie/popular")
        .then((res)=>{
         console.log(res);
+        dispatch(getApiConfigration(res));
        })
   }
 
   return (
     <>
-      <div className='App'> App</div>
+      <div className='App'> App
+      {url?.total_pages}
+      </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
-
+import { store } from './store/store'
+import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>   this cause 2 times api call to ensure that the api produces same result again
+   <Provider store={store}>
+     <App />
+     </Provider>
+  // </React.StrictMode>,
 )
