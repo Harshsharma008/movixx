144f2902c9e3997f49cc2e3d551a2f0314cc5399
Harshsharma008 <harshsharma01012002@gmail.com>
2023-12-20 16:00:21 +0530

    error fixing in trending

--- src/Components/carousel/Carousel.jsx
+++ src/Components/carousel/Carousel.jsx
@@ -10,13 +10,13 @@ import dayjs from "dayjs";
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
-// import CircleRating from "../circleRating/CircleRating";
+import CircleRating from "../circleRating/CircleRating";
 // import Genres from "../genres/Genres";
 
 import "./style.scss";
 
 const Carousel = ({ data, loading, endpoint, title }) => {
-    const carouselContainer = useRef(); // we can get the refrence of any div or other thing just by using "ref={carousalContainer}" , in react this helps for the selection 
+    const carouselContainer = useRef();
     const { url } = useSelector((state) => state.home);
     const navigate = useNavigate();
 
@@ -48,71 +48,73 @@ const Carousel = ({ data, loading, endpoint, title }) => {
 
     return (
         <div className="carousel">
-        <ContentWrapper>
-            {title && <div className="carouselTitle">{title}</div>}
-            <BsFillArrowLeftCircleFill
-                className="carouselLeftNav arrow"
-                onClick={() => navigation("left")}
-            />
-            <BsFillArrowRightCircleFill
-                className="carouselRighttNav arrow"
-                onClick={() => navigation("right")}
-            />
-            {!loading ? (
-                <div className="carouselItems" ref={carouselContainer}>
-                    {data?.map((item) => {
-                        const posterUrl = item.poster_path
-                            ? url.poster + item?.poster_path
-                            : PosterFallback;
-                        return (
-                            <div
-                                key={item.id}
-                                className="carouselItem"
-                                onClick={() =>
-                                    navigate(
-                                        `/${item.media_type || endpoint}/${
-                                            item.id
-                                        }`
-                                    )
-                                }
-                            >
-                                <div className="posterBlock">
-                                    <Img src={posterUrl} />
-                                    {/* <CircleRating
-                                        rating={item.vote_average.toFixed(
-                                            1
-                                        )}
-                                    /> */}
-                                    {/* <Genres
-                                        data={item.genre_ids.slice(0, 2)}
-                                    /> */}
+            <ContentWrapper>
+                {title && <div className="carouselTitle">{title}</div>}
+                <BsFillArrowLeftCircleFill
+                    className="carouselLeftNav arrow"
+                    onClick={() => navigation("left")}
+                />
+                <BsFillArrowRightCircleFill
+                    className="carouselRighttNav arrow"
+                    onClick={() => navigation("right")}
+                />
+                {!loading ? (
+                    <div className="carouselItems" ref={carouselContainer}>
+                        {data?.map((item) => {
+                            const posterUrl = item.poster_path
+                                ? url.poster + item.poster_path
+                                : PosterFallback;
+                                
+                            return (
+                                
+                                <div
+                                    key={item.id}
+                                    className="carouselItem"
+                                    onClick={() =>
+                                        navigate(
+                                            `/${item.media_type || endpoint}/${
+                                                item.id
+                                            }`
+                                        )
+                                    }
+                                >
+                                    <div className="posterBlock">
+                                   
+                                        <Img src={posterUrl} />
+                                        <CircleRating
+                                            rating={item.vote_average.toFixed(
+                                                1
+                                            )}
+                                        />
+                                        {/* <Genres
+                                            data={item.genre_ids.slice(0, 2)}
+                                        /> */}
+                                    </div>
+                                    <div className="textBlock">
+                                        <span className="title">
+                                            {item.title || item.name}
+                                        </span>
+                                        <span className="date">
+                                            {dayjs(item.release_date || item.first_air_date).format(
+                                                "MMM D, YYYY"
+                                            )}
+                                        </span>
+                                    </div>
                                 </div>
-                                <div className="textBlock">
-                                    <span className="title">
-                                        {item.title || item.name}
-                                    </span>
-                                    <span className="date">
-                                        {dayjs(item.release_date || item.first_air_date).format(
-                                            "MMM D, YYYY"
-                                        )}
-                                    </span>
-                                </div>
-                            </div>
-                        );
-                    })}
-                </div>
-            ) : (
-                <div className="loadingSkeleton">
-                    {skItem()}
-                    {skItem()}
-                    {skItem()}
-                    {skItem()}
-                    {skItem()}
-                </div>
-                // <span>Loading...</span>
-            )}
-        </ContentWrapper>
-    </div>
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <div className="loadingSkeleton">
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                    </div>
+                )}
+            </ContentWrapper>
+        </div>
     );
 };
 
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,26 +1,29 @@
-import React, {useState} from 'react'
-import ContentWrapper from '../../../Components/contentWrapper/ContentWrapper'
-import "../style.scss"
-import SwitchTabs from '../../../Components/switchTabs/SwitchTabs'
-import useFetch from '../../../hooks/UseFetch'
-import Carousel from '../../../Components/carousel/Carousel'
+import React, { useState } from "react";
+
+import Carousel from "../../../Components/carousel/Carousel";
+import ContentWrapper from "../../../Components/carousel/Carousel";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+
+import useFetch from "../../../hooks/useFetch";
+
 const Trending = () => {
-  const [endPoint, setEndPoint] = useState("day")
+    const [endpoint, setEndpoint] = useState("day");
 
-  const {data,loading} = useFetch(`/trending/all/${endPoint}`)
-  const onTabChange=(tab)=>{
-    setEndPoint(tab==="Day"? "day" : "week");
-  }
-  return (
-    <div className='carouselSection'>
-      <ContentWrapper>
-      <span className="carouselTitle">Trending</span>
-      <SwitchTabs data={[ "Day", "Week"]} onTabChange={onTabChange}/>
-      </ContentWrapper>
-      <Carousel data={data?.result} loading={loading}/>
-      
-      </div>
-  )
-}
+    const { data, loading } = useFetch(`/trending/movie/${endpoint}`);
 
-export default Trending
+    const onTabChange = (tab) => {
+        setEndpoint(tab === "Day" ? "day" : "week");
+    };
+
+    return (
+        <div className="carouselSection">
+            <ContentWrapper>
+                <span className="carouselTitle">Trending</span>
+                <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+            </ContentWrapper>
+            <Carousel data={data?.results} loading={loading} />
+        </div>
+    );
+};
+
+export default Trending;
