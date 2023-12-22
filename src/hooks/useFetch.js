be86a7cb560e361b8870aa8dc19bfca191985c66
Harshsharma008 <harshsharma01012002@gmail.com>
2023-12-22 17:02:18 +0530

    deployfix

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { fetchDataFromApi } from "../Utils/api";
+import { fetchDataFromApi } from "../utils/api";
 const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(null);
